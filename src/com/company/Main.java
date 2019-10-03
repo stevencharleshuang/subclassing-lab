5102237be8d29b12058f2451b380155eaa1f1e87
Steven Huang <steven.charles.huang@gmail.com>
2019-10-03 14:07:50 -0400

    completed base reqs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,10 +4,20 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
         Reptile gila = new Reptile(10, false, "Gila Monster", true);
+        Reptile lizard = new Reptile(8, false, "Lizard", false);
 
         System.out.println(gila.getName());
         System.out.println(gila.isVenomous());
+
+        Mammal rat = new Mammal(15, false, "Rat", true, "Squeak, Squeak");
+        Mammal cow = new Mammal(10, false, "Cow", false, "Moooooo");
+
+        System.out.println(rat.getName());
+        System.out.println(rat.isCarnivore());
+        System.out.println(rat.call());
+        System.out.println(cow.getName());
+        System.out.println(cow.isCarnivore());
+        System.out.println(cow.call());
     }
 }
--- src/com/company/Mammal.java
+++ src/com/company/Mammal.java
@@ -1,8 +1,33 @@
 package com.company;
 
 public class Mammal extends Animal {
-    public Mammal(int topSpeed, boolean isEndangered, String name) {
+    private boolean isCarnivore;
+    private String animalCall;
+
+    public Mammal(int topSpeed, boolean isEndangered, String name, boolean isCarnivore, String animalCall) {
         super(topSpeed, isEndangered, name);
 
+        this.isCarnivore = isCarnivore;
+        this.animalCall = animalCall;
+    }
+
+    public boolean isCarnivore() {
+        return isCarnivore;
+    }
+
+    public void setCarnivore(boolean carnivore) {
+        isCarnivore = carnivore;
+    }
+
+    public String getAnimalCall() {
+        return animalCall;
+    }
+
+    public void setAnimalCall(String animalCall) {
+        this.animalCall = animalCall;
+    }
+
+    public String call() {
+        return "The " + super.getName() + " says: " + animalCall + "!";
     }
 }
